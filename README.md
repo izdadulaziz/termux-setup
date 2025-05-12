8fecfebd5a757efffa4cbf8a6a1d97e624096bc2
Izdadul Aziz <izdadxtyz1@gmail.com>
2025-05-12 12:34:53 +0700

    add : file termux-setup.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # termux-setup
-setup for termux 
+setup for termux
+
+# instalation
+1. git clone https://github.com/izdadulaziz/termux-setup.git
+2. cd termux-setup
+3. bash termux-setup.sh
---
+++ termux-setup.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo -e "\npermission internal storage\n"
+
+termux-setup-storage
+
+echo -e "\nrun pkg update && pkg upgrade\n"
+
+pkg update -y && pkg upgrade 
+
+echo -e "\ninstall neovim\n"
+
+pkg install -y neovim
+
+echo -e "\ninstall wget\n"
+
+pkg install -y wget
+
+echo -e "\ninstall neofetch\n" 
+
+pkg install -y neofetch
+
+echo -e "\ninstall tree\n" 
+
+pkg install -y tree
+
+
